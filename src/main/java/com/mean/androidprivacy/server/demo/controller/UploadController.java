4c86e1fd5d3bfb20db0526a559dab029eee56cf3
MeanFan <meanfan@outlook.com>
2020-04-27 14:08:00 +0800

    show result on uploadStatus.html

--- src/main/java/com/mean/androidprivacy/server/demo/controller/UploadController.java
+++ src/main/java/com/mean/androidprivacy/server/demo/controller/UploadController.java
@@ -1,11 +1,17 @@
 package com.mean.androidprivacy.server.demo.controller;
 
+import com.mean.androidprivacy.server.demo.analysis.ApkAnalyzer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.xmlpull.v1.XmlPullParserException;
+import soot.jimple.infoflow.results.InfoflowResults;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -22,7 +28,7 @@ import java.nio.file.Paths;
 @Controller
 @RequestMapping("/")
 public class UploadController {
-    private static String UPLOADED_DIR_DEFAULT = "D://temp//";
+    private static String UPLOADED_DIR_DEFAULT = "D://APS//upload//";
 
     @GetMapping("/")
     public String index() {
@@ -38,12 +44,19 @@ public class UploadController {
 
         try {
             // Get the file and save it somewhere
-            byte[] bytes = file.getBytes();
             Path path = Paths.get(UPLOADED_DIR_DEFAULT + file.getOriginalFilename());
-            Files.write(path, bytes);
-
+            FileCopyUtils.copy(file.getInputStream(),new FileOutputStream(path.toFile()));
             redirectAttributes.addFlashAttribute("message",
                                                  "成功上传文件 '" + file.getOriginalFilename() + "'");
+            //TODO 分析文件
+            try {
+                InfoflowResults results = ApkAnalyzer.proc(path.toString());
+                if(results!=null){
+                    redirectAttributes.addFlashAttribute("results",results.toString());
+                }
+            } catch (XmlPullParserException e) {
+                e.printStackTrace();
+            }
 
         } catch (IOException e) {
             e.printStackTrace();
