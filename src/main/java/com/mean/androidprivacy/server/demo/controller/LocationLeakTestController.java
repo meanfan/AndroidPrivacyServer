9c1c676ed24e159e45ac097464746b413034bfe0
MeanFan <meanfan@outlook.com>
2020-05-18 00:03:27 +0800

    add LocationLeakTestController, only for test

---
+++ src/main/java/com/mean/androidprivacy/server/demo/controller/LocationLeakTestController.java
@@ -0,0 +1,24 @@
+package com.mean.androidprivacy.server.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ProjectName: AndroidPrivacyServer
+ * @ClassName: LocationLeakTestController
+ * @Description:
+ * @Author: MeanFan
+ * @Create: 2020-05-14 18:51
+ * @Version: 1.0
+ **/
+@RestController
+@RequestMapping("/leak/location")
+public class LocationLeakTestController {
+    @GetMapping
+    public String index(Double longitude,Double latitude){
+        System.out.println(String.format("LocationLeakTest: longitude %f,latitude %f",longitude,latitude));
+        return "success";
+    }
+}
